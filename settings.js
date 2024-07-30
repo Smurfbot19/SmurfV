ae93dd4d999d2daf293ceb1a138e267b0d1fa2e7
Smurfbot19 <fatkulrahman9a@gmail.com>
2024-07-30 11:55:44 +0700

    Smurf19

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6289666430345']
+global.botname = 'SmurfBot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Congela"
+global.sticker2 = "🌜"
